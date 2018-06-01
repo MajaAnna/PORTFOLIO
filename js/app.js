d29c177d3c4785fd55747d037ab86b2af19936e1
MajaAnna <majaprzyjazna@gmail.com>
2018-06-01 17:44:23 +0200

    validation

--- js/app.js
+++ js/app.js
@@ -3,9 +3,6 @@ $(function(){
     const hamburgerBtn = $('.icon-menu'),
           menu = $('.menu'),
           cancelBtn = $('.icon-cancel');
-
-          console.log(cancelBtn)
-
     menu.hide();
 
     hamburgerBtn.on('click', function(){
@@ -16,5 +13,51 @@ $(function(){
     cancelBtn.on('click', function(){
         menu.hide();
         hamburgerBtn.show();
+    })
+
+    // form validation
+    const form = $('form'),
+          name = $('#formName'),
+          surname = $('#formSurname'),
+          email = $('#formEmail'),
+          message = $('textarea'),
+          errorMsg = $('.errorMsg'),
+          modal = $('.modal');
+
+    form.on('submit', function(e){
+        e.preventDefault();
+
+        function errorMessage(content){
+            let contentErrorMsg = $('<p>');
+            errorMsg.append(contentErrorMsg);
+            contentErrorMsg.text(content);
+        }
+
+        let error = false;
+        errorMsg.html('');
+
+        if(name.val().length < 3){
+            error = true;
+            errorMessage('Your name should be longer than three letters!')
+        }
+
+        if(surname.val().length < 3){
+            error = true;
+            errorMessage('Your surname should be longer than three letters!')
+        }
+
+        if(email.val().indexOf('@'< 0)){
+            error = true;
+            errorMessage('Your email should have "@"!')
+        }
+
+        if(message.html().length <= 10){
+            error = true;
+            errorMessage('Your message should be longer than 10 letters!')
+        }
+
+        if(error=true){
+            e.preventDefault();
+        }
     })
 })
