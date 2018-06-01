a63b5d3cff9f2d3b659c11084b3cabc03366553d
MajaAnna <majaprzyjazna@gmail.com>
2018-06-01 12:48:44 +0200

    form and nav

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('hello')
+$(function(){
+    // navigation
+    const hamburgerBtn = $('.icon-menu'),
+          menu = $('.menu'),
+          cancelBtn = $('.icon-cancel');
+
+          console.log(cancelBtn)
+
+    menu.hide();
+
+    hamburgerBtn.on('click', function(){
+        $(this).hide();
+        menu.show();
+    })
+
+    cancelBtn.on('click', function(){
+        menu.hide();
+        hamburgerBtn.show();
+    })
+})
