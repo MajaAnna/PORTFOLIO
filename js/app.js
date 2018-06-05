9198d702415a8f12750089751d8e116569bca87a
MajaAnna <majaprzyjazna@gmail.com>
2018-06-05 14:08:57 +0200

    navigation, scrolling, gallery

--- js/app.js
+++ js/app.js
@@ -70,5 +70,47 @@ $(function(){
         }       
     })
 
+    //COLLAGES
+    let collageSectionPositionTop=$('#collageSection').position().top;
+    let formSectionPositionBottom=$('#formSection').position().top;
+    const nav = $('nav');
+
+    //nav
+    $(window).on('scroll', function() {   
+        menu.hide();
+        hamburgerBtn.show();
+        
+        let scrollTop = $(window).scrollTop();
+        if (scrollTop >= collageSectionPositionTop-100) {
+            nav.addClass('navScrolling');
+        } else {
+            nav.removeClass('navScrolling');
+        }
+
+        if (scrollTop >= collageSectionPositionTop+100) {
+            nav.removeClass('navScrolling');
+        }
+    });
+
+    //sizing
+    const pictures = $('.pictures');
+    let ulHeight = $('.pictures ul').height();
+    let picturesHeight = ulHeight/7
+    pictures.css('height', picturesHeight)
+
+    $(window).on('resize', function(){
+        let ulHeight = $('.pictures ul').height();
+        let picturesHeight = ulHeight/7
+        pictures.css('height', picturesHeight)
+    })
+
+    //buttons
+    const nextBtn = $('button .icon-right-open-big')
+    const prevBtn = $('button .icon-left-open-big')
+
+    prevBtn.on('click', function(){
+        console.log('click')
+    })
+
     
 })
